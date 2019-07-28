891f540b2731c9f46d5216388d5e4c13464db353
Satrajit Chatterjee <satrajit2016@gmail.com>
2019-07-28 01:39:57 -0400

    Made website semi-responsive

---
+++ script.js
@@ -0,0 +1,20 @@
+// /*
+// if (screen.width * 1.0/ screen.height < 4.0/3.0) {
+//     document.location = "mobile.html";
+// }
+// */
+
+// var elem = document.createElement("img");
+// elem.src = 'images/parallax1.png';
+
+// if (document.documentElement.clientWidth < 900) {
+//     console.log("changing");
+
+//     document.getElementById("home").classList.remove('bgimg-1');
+//     document.getElementById("home").classList.remove('w3-display-container');
+//     document.getElementById("home").classList.remove('w3-opacity-min');
+
+//     document.getElementById("home").classList.add('img-fluid');
+//     document.getElementById("home").appendChild(elem);
+
+// } 
