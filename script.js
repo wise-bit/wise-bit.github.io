07275638e219c31769d0e26d0040431ee18fac4f
wise-bit <satrajit2016@gmail.com>
2021-12-13 02:44:28 -0500

    mini website revamp

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
+var darkMode = false;
+
 jQuery(document).ready(function($) {
     $(".clickable-content").click(function() {
         window.location = $(this).data("href");
     });
+
+    $('#moon').click(function() {
+		if (darkMode) {
+			$(':root').css({
+		        '--main-bg-color': 'white',
+		        '--main-fg-color': 'black'
+		    });
+		    darkMode = false;
+		} else {
+			$(':root').css({
+		        '--main-bg-color': 'black',
+		        '--main-fg-color': 'white'
+		    });
+		    darkMode = true;
+		}
+	});
 });
 
 $(document).ready(function(){
