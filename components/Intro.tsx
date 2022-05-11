5f9d4d0c4acdfcad1b6a444831891fc82eac95ce
wise-bit <satrajit2016@gmail.com>
2022-05-11 17:05:56 -0400

    added resume button

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -15,6 +15,13 @@ const Intro: NextPage<Props> = (props) => {
         Hi I&apos;m <span className={"highlight"}>{username}</span> !
       </h1>
       <h3>Computer Science and Mathematics Undergrad Student</h3>
+
+      <Link href="./files/CV.pdf">
+      <h2 className={"highlight clickable"} style={{ backgroundColor: "#326340" }}>
+        Résumé
+      </h2>
+      </Link>
+
       <h3 style={{ color: "#777777", marginTop: "100px" }}>
         <span className={"highlight2"}>v3</span> is still under development,{" "}
         <Link href="/legacy">
