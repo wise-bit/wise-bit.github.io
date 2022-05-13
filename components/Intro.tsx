073830863a2ab9399e63f4a07ab6a2b363910dee
wise-bit <satrajit2016@gmail.com>
2022-05-13 14:58:20 -0400

    added important buttons and removed redundant space

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,6 +1,6 @@
-import { NextPage } from "next";
-import Link from "next/link";
-import styles from "../styles/Home.module.css";
+import { NextPage } from 'next';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
 
 interface Props {
   username: string;
@@ -12,20 +12,69 @@ const Intro: NextPage<Props> = (props) => {
   return (
     <>
       <h1 className={styles.title}>
-        Hi I&apos;m <span className={"highlight"}>{username}</span> !
+        Hi I&apos;m <span className={'highlight'}>{username}</span> !
       </h1>
-      <h3>Computer Science and Mathematics Undergrad Student</h3>
+      <h3 style={{ textAlign: 'center', marginBottom: '50px' }}>
+        Computer Science and Mathematics Undergrad Student
+      </h3>
 
-      <Link href="./files/CV.pdf">
-      <h2 className={"highlight clickable"} style={{ backgroundColor: "#326340" }}>
-        Résumé
-      </h2>
-      </Link>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <div style={{ margin: '20px' }}>
+          <Link href='https://github.com/wise-bit'>
+            <h2
+              className={'highlight clickable'}
+              style={{ backgroundColor: '#cc5850', width: '150px' }}
+            >
+              Github
+            </h2>
+          </Link>
+        </div>
+        <div style={{ margin: '20px' }}>
+          <Link href='https://www.linkedin.com/in/satrajit-c'>
+            <h2
+              className={'highlight clickable'}
+              style={{ backgroundColor: '#326340', width: '150px' }}
+            >
+              LinkedIn
+            </h2>
+          </Link>
+        </div>
+      </div>
 
-      <h3 style={{ color: "#777777", marginTop: "100px" }}>
-        <span className={"highlight2"}>v3</span> is still under development,{" "}
-        <Link href="/legacy">
-          <span className={"highlight2 clickable"}>check out v2</span>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          marginTop: '0px',
+          height: 'auto',
+        }}
+      >
+        <div style={{ margin: '20px' }}>
+          <Link href='./files/CV.pdf'>
+            <h2
+              className={'highlight clickable'}
+              style={{ backgroundColor: '#509fcc', width: '150px' }}
+            >
+              Résumé
+            </h2>
+          </Link>
+        </div>
+        <div style={{ margin: '20px' }}>
+          <Link href='mailto:satrajit314@gmail.com'>
+            <h2
+              className={'highlight clickable'}
+              style={{ backgroundColor: '#bab759', width: '150px' }}
+            >
+              Email
+            </h2>
+          </Link>
+        </div>
+      </div>
+
+      <h3 style={{ color: '#777777', marginTop: '100px', textAlign: 'center' }}>
+        <span className={'highlight2'}>v3</span> is still under development,{' '}
+        <Link href='/legacy'>
+          <span className={'highlight2 clickable'}>check out v2</span>
         </Link>
       </h3>
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,20 @@
-import type { NextPage } from "next";
-import Head from "next/head";
+import type { NextPage } from 'next';
+import Head from 'next/head';
 // import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import Intro from "../components/Intro";
+import styles from '../styles/Home.module.css';
+import Intro from '../components/Intro';
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
         <title>Satrajit Chatterjee</title>
-        <meta name="description" content="By Satrajit Chatterjee" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='description' content='By Satrajit Chatterjee' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main className={styles.main}>
-        <Intro username="Sat" />
+        <Intro username='Sat' />
       </main>
     </div>
   );
