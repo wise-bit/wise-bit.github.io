09b2ea82f53c37b986483cd4981792e69dfa894f
wise-bit <satrajit2016@gmail.com>
2025-07-14 18:04:46 -0400

    fix: phone view

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ const Home: NextPage = () => {
       <Head>
         <title>Satrajit Chatterjee</title>
         <meta name='description' content='Satrajit Chatterjee' />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
